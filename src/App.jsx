0ed439c54e78e341ea110721684d4dfda4d4c052
meethatkar <meethatkar000@gmail.com>
2025-11-10 01:16:50 +0530

    done with landing page added hover effect too

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar"
 function App() {
 
   return (
-    <div className="w-full h-screen bg-zinc-900 text-white px-[4vw]">
+    <div className="w-full h-screen bg-zinc-900 text-white ">
       <Navbar/>
       <Landing />
     </div>
--- src/components/Landing .jsx
+++ src/components/Landing .jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div className="textstructure h-screen w-full pt-1">
+    <div className="textstructure h-screen w-full pt-1 px-[4vw] overflow-x-hidden">
       {/*  
       (TRY TO REMOVE pt-1 FROM textstructure, YOU"LL SEE THE ISSUE)
       🔹 Margin vs Padding (Gap Color Issue)
@@ -17,9 +17,22 @@ const Landing = () => {
        to appear in the same background color as the parent.
         */}
       <div className="masker mt-50">
-        {["we create", "eye opening", "presentations"].map((item, index)=>(
+        {["we create", "eye opening", "presentations"].map((item, index) => (
           <h1 key={index} className="font-['Test Founders Grotesk X-Cond-SmBd'] text-[7vmax] uppercase font-medium leading-[6vmax] tracking-tight"> {item} </h1>
         ))}
+      </div>
+
+      <span className='inline-block mt-[10%] w-full scale-120 h-[1px] bg-white/60' />
+      <div id="feature" className='flex items-center w-full mt-[1%]'>
+        <div id="text" className='flex w-[70%] items-center justify-between'>
+          {["Presentation and sotrytelling agency", "For innovation teams and global brands"].map((item) => (
+            <p>{item}</p>
+          ))}
+        </div>
+        <button id="ctc" className='w-[30%] gap-[5%] flex items-center justify-end group'>
+          <span className='border-[1px] rounded-4xl px-4 py-1 group-hover:bg-white group-hover:text-black transition-all ease-linear duration-200 cursor-pointer'>START THE PROJECT</span>
+          <span className='relative'> <i class="relative ri-arrow-right-up-line text-2xl rounded-full border-[2px] p-1 group-hover:text-black z-10"></i> <span className='inline-block w-full h-full bg-white scale-0 absolute top-0 left-0 rounded-full duration-300 ease-linear group-hover:scale-100'></span> </span>
+        </button>
       </div>
     </div>
   )
